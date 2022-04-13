55bdb506fb0fbfa0dbb6942865bcd2420aae44a0
aminbaig <aminbaig@hotmail.com>
2022-04-13 13:55:29 +0500

    jwt authentication added

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+const passport = require("passport");
 
+const app = express();
 
-const app = express()
+// bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Bring in DB config
-const db = require('./config/keys').mongoURI
+const db = require("./config/keys").mongoURI;
 
 //Connect to mongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log('Connected to mongoDB'))
-    .catch(err => console.log(err))
+  .connect(db)
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello'))
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
 
 // Use routes
-app.use('/api/users', users)
-app.use('/api/profile', profile)
-app.use('/api/posts', posts)
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
