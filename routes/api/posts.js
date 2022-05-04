8ecd3951b61cf86ec53df0f732d47ad74193147a
Amin Baig <aminbaig@hotmail.com>
2022-05-04 12:09:18 +0500

    Added create a new post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,35 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-router.get('/test', (req, res) => res.json({msg: "Posts works"}))
+//Bring in posts model
+const Post = require("../../models/Post");
 
-module.exports = router
+// Bring in Post Validator
+const validatePostInput = require("../../validation/post");
+
+router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    // Check Validation
+    if (!isValid) {
+      // if any errors send 400 with errors object
+      return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id,
+    });
+
+    newPost.save().then((post) => res.json(post));
+  }
+);
+
+module.exports = router;
